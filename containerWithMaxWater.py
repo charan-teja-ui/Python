10d3ec849d8fc8044fa58ed9bbdb4969c1212f3f
charan_teja <jazzcharan758@gmail.com>
2026-08-12 21:33:58 +0530

    Create containerWithMaxWater.py

---
+++ containerWithMaxWater.py
@@ -0,0 +1,20 @@
+
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        water = 0
+        left = 0
+        right = len(height) - 1
+
+        while left < right:
+            water = max(
+                water,
+                min(height[left], height[right]) * (right - left)
+            )
+
+            if height[left] > height[right]:
+                right -= 1
+            else:
+                left += 1
+
+        return water
